eff72a012a4c7261bd32d40d2b007bf2a539f53f
vladji <vladjislav@gmail.com>
2019-11-18 19:10:56 +0300

    oauth v2 build

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -5,16 +5,29 @@ export default function authentification() {
   const loginBtn = document.querySelector('.login');
   const outputText = document.querySelector('.login-output');
 
+  const request = async (dataToken) => {
+    const resp = await fetch('https://api.github.com/user',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `token ${dataToken}`,
+        },
+      });
+    console.log('resp', resp);
+  }
+
   loginBtn.addEventListener('click', () => {
     const auth = new Netlify({});
 
-    const req = auth.authenticate({ provider: 'github', scope: 'user' }, (err, data) => {
+    auth.authenticate({ provider: 'github', scope: 'user' }, (err, data) => {
       if (err) {
         outputText.innerText = `Error Authenticating with GitHub: ${err}`;
       } else {
-        outputText.innerText = `Authenticated with GitHub. Access Token: ${data.token}`;
+        // outputText.innerText = `Authenticated with GitHub. Access Token: ${data.token}`;
+        console.log('data.token', data.token);
+        console.log('data', data);
+        request(data.token);
       }
     });
-    console.log('req', req);
   });
 }
