b35cad357c273e5a1bd14770398eeb4d23e00858
vladji <vladjislav@gmail.com>
2019-11-18 19:42:37 +0300

    show user info

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,6 +4,24 @@ import Netlify from '../../node_modules/netlify-auth-providers/lib/netlify';
 export default function authentification() {
   const loginBtn = document.querySelector('.login');
   const outputText = document.querySelector('.login-output');
+  const header = document.querySelector('.header');
+
+  const showInfo = (name, ava) => {
+    loginBtn.hidden = true;
+    const infoWrap = document.createElement('div');
+    const nameBlock = document.createElement('p');
+    const avaBlock = document.createElement('img');
+
+    infoWrap.classList.add('infoWrap');
+    avaBlock.classList.add('user-ava');
+
+    nameBlock.innerHTML = `Hello, ${name}`;
+    avaBlock.src = ava;
+    avaBlock.alt = `${name} avatar`;
+
+    infoWrap.append(nameBlock, avaBlock);
+    header.append(infoWrap);
+  }
 
   const request = async (dataToken) => {
     const resp = await fetch('https://api.github.com/user',
@@ -13,9 +31,12 @@ export default function authentification() {
           Authorization: `token ${dataToken}`,
         },
       });
-    console.log('resp', resp);
     const userData = await resp.json();
     console.log('userData', userData);
+    const userName = userData.name;
+    const userAva = userData.avatar_url;
+
+    showInfo(userName, userAva);
   };
 
   loginBtn.addEventListener('click', () => {
